1cd0d12faf3cf2a5faf3c456716ba5555671ae63
Psicodelico <guosj921@163.com>
2020-04-09 12:34:02 +0800

    模块化路由

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
+const controller = require('./controller');
 
 // log request URL:
 app.use(async (ctx, next) => {
@@ -13,29 +14,8 @@ app.use(async (ctx, next) => {
 // 由于middleware的顺序很重要，这个koa-bodyparser必须在router之前被注册到app对象上
 app.use(bodyParser());
 
-router.get('/', async (ctx, next) => {
-  ctx.response.body = `<h1>Index</h1>
-      <form action="/signin" method="post">
-          <p>Name: <input name="name" value="koa"></p>
-          <p>Password: <input name="password" type="password"></p>
-          <p><input type="submit" value="Submit"></p>
-      </form>`;
-});
-
-router.post('/signin', async (ctx, next) => {
-  var name = ctx.request.body.name || '',
-      password = ctx.request.body.password || '';
-  console.log(`signin with name: ${name}, password: ${password}`);
-  if (name === 'koa' && password === '12345') {
-      ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
-  } else {
-      ctx.response.body = `<h1>Login failed!</h1>
-      <p><a href="/">Try again</a></p>`;
-  }
-});
-
 // add router middleware:
-app.use(router.routes());
+app.use(controller());
 
 app.listen(3000);
 console.log('app started at port 3000...');
