4581fac48acbae0ecdb178b570022a10cb4e6184
Psicodelico <guosj921@163.com>
2020-04-09 14:14:36 +0800

    防注入

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
-const Koa = require('koa');
-// 注意require('koa-router')返回的是函数:
-const router = require('koa-router')();
-const bodyParser = require('koa-bodyparser');
+const nunjucks = require('nunjucks');
 
-const app = new Koa();
-const controller = require('./controller');
+function createEnv(path, opts) {
+    var autoescape = opts.autoescape === undefined ? true : opts.autoescape,
+        noCache = opts.noCache || false,
+        watch = opts.watch || false,
+        throwOnUndefined = opts.throwOnUndefined || false,
+        env = new nunjucks.Environment(
+            new nunjucks.FileSystemLoader('views', {
+                noCache: noCache,
+                watch: watch,
+            }), {
+                autoescape: autoescape,
+                throwOnUndefined: throwOnUndefined
+            });
+    if (opts.filters) {
+        for (var f in opts.filters) {
+            env.addFilter(f, opts.filters[f]);
+        }
+    }
+    return env;
+}
 
-// log request URL:
-app.use(async (ctx, next) => {
-    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
-    await next();
+var env = createEnv('views', {
+    watch: true,
+    filters: {
+        hex: function (n) {
+            return '0x' + n.toString(16);
+        }
+    }
 });
-// 由于middleware的顺序很重要，这个koa-bodyparser必须在router之前被注册到app对象上
-app.use(bodyParser());
 
-// add router middleware:
-app.use(controller());
-
-app.listen(3000);
-console.log('app started at port 3000...');
+var s = env.render('hello.html', { name: '<script>alert("小明")</script>' });
+console.log(s);
