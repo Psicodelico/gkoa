9b3d3853a6e6b1dd700799d8b147a5f12b151df9
Psicodelico <guosj921@163.com>
2020-04-11 13:31:09 +0800

    signin 路由

--- controllers/hello.js
+++ controllers/hello.js
@@ -1,8 +1,19 @@
-var fn_hello = async (ctx, next) => {
-    var name = ctx.params.name;
-    ctx.response.body = `<h1>Hello, ${name}!</h1>`;
-};
-
 module.exports = {
-    'GET /hello/:name': fn_hello
+    'POST /signin': async (ctx, next) => {
+        var
+            email = ctx.request.body.email || '',
+            password = ctx.request.body.password || '';
+        if (email === 'admin@example.com' && password === '123456') {
+            console.log('signin ok!');
+            ctx.render('signin-ok.html', {
+                title: 'Sign In OK',
+                name: 'Mr Node'
+            });
+        } else {
+            console.log('signin failed!');
+            ctx.render('signin-failed.html', {
+                title: 'Sign In Failed'
+            });
+        }
+    }
 };
