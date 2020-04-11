dc18c65a2cae8d068d90c77c6033a5774fceaedb
Psicodelico <guosj921@163.com>
2020-04-11 13:20:17 +0800

    isProduction

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('koa-bodyparser');
 const app = new Koa();
 
 const controller = require('./controller');
-const staticFiles = require('./static-files');
+
 const templating = require('./templating');
 
 const isProduction = process.env.NODE_ENV === 'production';
@@ -22,7 +22,11 @@ app.use(bodyParser());
 // add router middleware:
 app.use(controller());
 
-app.use(staticFiles('/static/', __dirname + '/static'));
+if (! isProduction) {
+    // 这是因为在生产环境下，静态文件是由部署在最前面的反向代理服务器（如Nginx）处理的，Node程序不需要处理静态文件。而在开发环境下，我们希望koa能顺带处理静态文件，否则，就必须手动配置一个反向代理服务器，这样会导致开发环境非常复杂。
+    const staticFiles = require('./static-files');
+    app.use(staticFiles('/static/', __dirname + '/static'));
+}
 
 app.use(templating('views', {
     noCache: !isProduction,
